892b26fe298e85bc2cd10b9e2f60f88812b65ed6
Jacoba <Junior1232131@gmail.com>
2023-10-11 11:22:52 -0300

    script improvements

--- makefile/run.sh
+++ makefile/run.sh
@@ -1,1 +1,21 @@
-gcc -c view.c model.c handleFunc.c studentFunc.c && gcc view.o model.o handleFunc.o studentFunc.o -o database && ./database
+#!/bin/bash
+
+# Define variables for source code files and executable
+source_dir="C:/Users/Jacoba/Documents/Coding/C/Projetos/Banco_De_Dados/src"
+source_files=("view.c" "model.c" "handleFunc.c" "studentFunc.c")
+output_dir="C:/Users/Jacoba/Documents/Coding/C/Projetos/Banco_De_Dados/src"
+executable="database"
+
+# Change to the source directory
+cd "$source_dir" || { echo "Error: Unable to change to the source directory."; exit 1; }
+
+# Compile the source files individually and check for errors
+for file in "${source_files[@]}"; do
+  gcc -c "$file" || { echo "Error: Compilation of $file failed."; exit 1; }
+done
+
+# Link the object files and create the executable
+gcc *.o -o "$output_dir/$executable" || { echo "Error: Linking object files failed."; exit 1; }
+
+# Execute the program
+"$output_dir/$executable"
